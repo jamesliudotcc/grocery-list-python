a16a71607c78c9da2fd6f894d03207fae1b42f41
jamesliudotcc <james@jamesliu.cc>
2019-02-26 11:02:21 -0800

    CORS error with frontend fixed

--- grocerylist/settings.py
+++ grocerylist/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "graphene_django",
     "lists",
 ]
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +124,4 @@ STATIC_URL = "/static/"
 
 GRAPHENE = {"SCHEMA": "grocerylist.schema.schema"}
 
+CORS_ORIGIN_WHITELIST = ("localhost:3000", "null")
--- lists/schema.py
+++ lists/schema.py
@@ -68,30 +68,6 @@ class Query(graphene.ObjectType):
             return House.objects.get(pk=id)
 
 
-class CreateLink(graphene.Mutation):
-    id = graphene.Int()
-    url = graphene.String()
-    description = graphene.String()
-    posted_by = graphene.Field(UserType)
-
-    class Arguments:
-        url = graphene.String()
-        description = graphene.String()
-
-    def mutate(self, info, url, description):
-        user = info.context.user or None
-
-        link = Link(url=url, description=description, posted_by=user)
-        link.save()
-
-        return CreateLink(
-            id=link.id,
-            url=link.url,
-            description=link.description,
-            posted_by=link.posted_by,
-        )
-
-
 class CreateItem(graphene.Mutation):
     id = graphene.Int()
     house = graphene.Int()
@@ -150,6 +126,5 @@ class PurchaseItem(graphene.Mutation):
 
 
 class Mutation(graphene.ObjectType):
-    create_link = CreateLink.Field()
     create_item = CreateItem.Field()
     purchase_item = PurchaseItem.Field()
